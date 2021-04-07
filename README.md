0ef5bf8e62c163a2aec0443d4ec177fd77e40cc8
Oskar Lindgren <oskar.lindgren@asurion.com>
2021-04-07 23:38:06 +0900

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Japanese Numbers :jp:
+CLI for learning Japanese numbers
+
+Listen to the spoken number and write your guess.
+
+## Prerequisites
+This app has been developed and tested on MacOS Catalina.
+
+Make sure that `say` command is present and that the voice `Kyoko` is listed.
+```
+$ say -v ? | grep Kyoko
+```
+Expected output:
+```
+Kyoko               ja_JP    # こんにちは、私の名前はKyokoです。日本語の音声をお届けします。
+```
+
+## Usage
+
+Use `start.sh` and input the max number to practice with.
+```
+$ ./start.sh MAX_NUMBER
+```
+
+## Options
+* `-nvf, --no-voice-feedback` - No Voice Feedback after guessed a number.
+* `f, --fast-mode` - Get a new number directly after guessed a number.
+* `sv, --slow-voice` - Slow down voice rate when speaking a number.
+
+## Gameplay
+
+```
+
+   /) /)
+  ( ^.^ )
+ C(") (")
+
+----------------------------
+Welcome to Japanese Numbers!
+----------------------------
+Stats: 1/1
+Success Rate: 100%
+
+✅ 82
+
+Press ENTER for next number
+```
+
+## Blog Post
+https://www.oskarlindgren.se/blog/making-a-deno-cli-learning-japanese-numbers/
---
+++ main.js
@@ -0,0 +1,94 @@
+let correct = 0;
+let wrong = 0;
+let nextNumber = 0;
+let isGuessing = false;
+
+document.getElementById('stats').innerHTML = '0/0';
+document.getElementById('rate').innerHTML = '0%';
+
+document.getElementById('enter').addEventListener('click', function (event) {
+	event.preventDefault();
+  enter();
+});
+
+document.getElementById('guessInput').addEventListener('keypress', function (event) {
+  if (event.key === 'Enter' || event.keyCode === 13) {
+    const numberGuess = document.getElementById('guessInput').value;
+    if (numberGuess) {
+      guess();
+    } else {
+      enter();
+    }
+  }
+});
+
+function enter() {
+  const maxNumber = document.getElementById('maxNumber').value;
+  if (!maxNumber) {
+    alert('Please enter Max Number');
+    return;
+  }
+
+  document.getElementById('message').innerHTML = 'What is the number? (press ENTER to listen again)';
+
+  const numberGuess = document.getElementById('guessInput').value;
+  if (!numberGuess && isGuessing) {
+    speak(nextNumber);
+  } else {
+
+    nextNumber = Math.round(Math.random() * maxNumber);
+    isGuessing = true;
+    document.getElementById('result').innerHTML = '&nbsp;'
+    speak(nextNumber);
+  }
+}
+
+const synth = window.speechSynthesis;
+
+function speak(text) {
+  // console.log('window.speechSynthesis', window.speechSynthesis)
+  var utterThis = new SpeechSynthesisUtterance(text);
+  voices = synth.getVoices();
+  voices.forEach(x => {
+    if (x.lang.includes('ja')) {
+      utterThis.voice = x;
+    }
+  })
+
+
+  synth.speak(utterThis);
+}
+
+function guess() {
+  const numberGuess = document.getElementById('guessInput').value;
+
+  const result = numberGuess == nextNumber;
+  if (result) {
+    correct++;
+    document.getElementById('result').innerHTML = `✅ ${nextNumber}`
+    speak('正解');
+  } else {
+    wrong++;
+    document.getElementById('result').innerHTML = `❌ correct number: ${nextNumber}`;
+    speak('違う');
+  }
+  isGuessing = false;
+  document.getElementById('message').innerHTML = 'Press ENTER for next number';
+  document.getElementById('guessInput').value = '';
+
+  const totalRounds = correct + wrong;
+  const successRate = Math.round((correct / totalRounds) * 100);
+  document.getElementById('stats').innerHTML = `${correct}/${totalRounds}`;
+  document.getElementById('rate').innerHTML = `${successRate}%`;
+}
+
+document.getElementById('guess').addEventListener('click', function (event) {
+	event.preventDefault();
+  guess();
+});
+
+console.log(`
+  (\\-/)
+ (='.'=)
+ (")-(")o
+`);
